58ef3c90ae2934f281b5d8fae0b45044ea4d1cf1
pacman-cli <puspopuspo520@gmail.com>
2026-02-25 04:56:47 +0600

    Added: Kafka config {consumer factory, kafka listener container factory}

--- notification-service/src/main/java/org/example/notificationservice/config/KafkaConfig.java
+++ notification-service/src/main/java/org/example/notificationservice/config/KafkaConfig.java
@@ -1,4 +1,40 @@
 package org.example.notificationservice.config;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConfig {
+    private final ConsumerFactory consumerFactory;
+
+    public KafkaConfig(ConsumerFactory consumerFactory) {
+        this.consumerFactory = consumerFactory;
+    }
+
+    // need two beans : Consumer Factory and kafka Listener Container Factory
+    public ConsumerFactory<String, Object> consumerFactory(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG,"notification-service");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
