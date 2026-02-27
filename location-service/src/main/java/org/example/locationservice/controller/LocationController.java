bf0cf7537c4f60ab7a52ecae1826ab679a26c466
pacman-cli <puspopuspo520@gmail.com>
2026-02-27 10:02:05 +0600

    Added sos event consumer

--- location-service/src/main/java/org/example/locationservice/controller/LocationController.java
+++ location-service/src/main/java/org/example/locationservice/controller/LocationController.java
@@ -1,15 +1,14 @@
 package org.example.locationservice.controller;
 
-import org.example.locationservice.model.LocationPing;
+import java.util.Map;
+import java.util.UUID;
+
 import org.example.locationservice.service.LocationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/location")
@@ -24,13 +23,13 @@ public class LocationController {
      * Updates location and returns a success message
      */
     @PostMapping("/update")
-    public ResponseEntity<String> updateLocation(@RequestBody Map<String,Object> body){
+    public ResponseEntity<String> updateLocation(@RequestBody Map<String, Object> body) {
         UUID sosId = UUID.fromString((String) body.get("sosId"));
         UUID userId = UUID.fromString((String) body.get("userId"));
         double lat = (Double) body.get("lat");
         double lng = (Double) body.get("lng");
 
-        locationService.updateLocation(sosId,userId,lat,lng);
+        locationService.updateLocation(sosId, userId, lat, lng);
         return ResponseEntity.ok().body("Location updated successfully");
     }
 }
--- notification-service/src/main/java/org/example/notificationservice/kafka/SosEventConsumer.java
+++ notification-service/src/main/java/org/example/notificationservice/kafka/SosEventConsumer.java
@@ -1,4 +1,20 @@
 package org.example.notificationservice.kafka;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SosEventConsumer {
+    @KafkaListener(topics = "sos-event")
+    public void consume(){
+
+    }
+
+    private void handleSosTriggered(){
+
+    }
+
+    private void handleSosCancelled(){
+
+    }
 }
