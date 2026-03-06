9d231d1ed862fff08e421f0c7f07fd68916c7d03
pacman-cli <puspopuspo520@gmail.com>
2026-03-06 15:18:52 +0600

    Added kafka consumer

--- location-service/src/main/java/org/example/locationservice/kafka/LocationEventProducer.java
+++ location-service/src/main/java/org/example/locationservice/kafka/LocationEventProducer.java
@@ -1,17 +1,36 @@
 package org.example.locationservice.kafka;
 
-import lombok.RequiredArgsConstructor;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.example.locationservice.model.LocationPing;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LocationEventProducer {
-    private final KafkaTemplate<String,Object> kafkaTemplate;
-    private final static String TOPIC="location.update";
 
-    public void publishLocation(LocationPing ping){
-        kafkaTemplate.send(TOPIC,ping.getSosId().toString(),ping);
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${location.kafka.topic}")
+    private String locationTopic;
+
+    public void publishLocation(LocationPing ping) {
+        Map<String, Object> event = new HashMap<>();
+        event.put("eventType", "LOCATION_UPDATED");
+        event.put("sosId", ping.getSosId().toString());
+        event.put("userId", ping.getUserId().toString());
+        event.put("lat", ping.getLat());
+        event.put("lng", ping.getLng());
+        event.put("timestamp", ping.getTimestamp().toString());
+
+        kafkaTemplate.send(locationTopic, ping.getSosId().toString(), event);
+        log.debug("Published LOCATION_UPDATED event for sosId={}, userId={}", ping.getSosId(), ping.getUserId());
     }
 }
